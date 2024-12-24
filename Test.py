d3a6903943dd3b9bb568912dc153f11aa984fe9e
Esentur0101k <nollal22@gmail.com>
2024-12-24 18:51:52 +0300

    rgwargfwe

---
+++ Test.py
@@ -0,0 +1,22 @@
+
+
+
+
+
+
+
+
+################## git Githab киришуу ####################
+
+#   1 git init (гитти инициализация кылуу )
+
+
+#   2  git add .   / либо по названию файла
+
+#   3    git commit -m "men registration buttum
+
+#   4  git branch -M main
+
+#   5 git remote add origin https://github.com/Esentur0101k/leson-.git
+
+#   6 git push -u origin main
